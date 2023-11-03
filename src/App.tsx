efddb03d59455b19c36dcbe8c87ceef78789e74a
Dhunu <angelsaikia333@gmail.com>
2023-11-03 11:34:30 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./globals.css";
+
+function App() {
+    return (
+        <>
+            <h1 className="w-full h-screen flex justify-center items-center text-3xl font-bold">Comming Soon ...</h1>
+        </>
+    );
+}
+
+export default App;
